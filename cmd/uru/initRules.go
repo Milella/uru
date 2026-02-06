a6cf6fabb40e5426cfd5e64993fc1d95a30769ae
Scott Milella <milella@hotmail.com>
2026-02-06 14:55:41 -0800

    Fixed the project so it will create the MacOS binaries (both ARM and X86) and it will create a Linux Binary via github actions

---
+++ cmd/uru/initRules.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/milella/uru/internal/env"
+
+// initRules is required by main.go but was never implemented.
+// This no-op implementation satisfies all platforms.
+func initRules(ctx *env.Context) {
+	// Nothing needed here yet.
+}
